134ae9a9d5e345ae071b511e8c6bf73c670f1a2f
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-02 15:15:01 +0000

    Fixed binaryAgent

---
+++ Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/Binary Agent.js
@@ -0,0 +1,20 @@
+// Return an English translated sentence of the passed binary string.
+// The binary string will be space separated.
+
+/**
+* A method that returns an English translated sentence of the passed binary string.
+* The binary string will be space separated.
+* @param `string` - The binary string
+* @returns `string` - The English translation
+*/
+const binaryAgent = str => {
+  let arr = new Array()
+  
+  str.split(' ').forEach(function(element){
+    arr.push(String.fromCharCode(parseInt(element, 2)))
+  })
+  
+  return arr.join('')
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111") //?
