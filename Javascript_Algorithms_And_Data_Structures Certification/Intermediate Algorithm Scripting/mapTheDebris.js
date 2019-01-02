a8acd98ce730998a7d614a7734b5dae82d5e51e6
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-02 20:04:42 +0000

    mapTheDebris

--- Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/mapTheDebris.js
+++ Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/mapTheDebris.js
@@ -10,12 +10,30 @@
 * @param `array` - An array of objects
 * @returns `array` - An array of objects, conatining the Orbital Period
 */
-const orbitalPeriod = arr => {
-  var GM = 398600.4418
-  var earthRadius = 6367.4447
-  let alt = 35873.5553
-  let answer = 0
-  return answer
+// const orbitalPeriod = arr => {
+//   var GM = 398600.4418
+//   var earthRadius = 6367.4447
+//   let alt = 35873.5553
+//   let answer = 0
+//   var oPeriod2 = Math.ceil(((2*Math.PI) * totalRadius) / (Math.sqrt(GM / (earthRadius + satelliteAlt)))) //?
+//   return answer
+// }
+
+function orbitalPeriod(satellites) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return satellites.map(function(data, i, arr) {
+    var satelliteAlt = arr[i].avgAlt;
+    var totalRadius = earthRadius + satelliteAlt;
+    var orbitalV = Math.sqrt(GM / totalRadius);
+    var oPeriod = Math.round(((2 * Math.PI) * totalRadius) / orbitalV);
+    arr[i].velocity = orbitalV.toFixed(4);
+    arr[i].orbitalPeriod = oPeriod;
+    return {
+      name: data.name,
+      orbitalPeriod: data.orbitalPeriod
+    };
+  });
 }
 
 // Expects [{name: "sputnik", orbitalPeriod: 86400}]
