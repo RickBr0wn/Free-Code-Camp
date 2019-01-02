746b56a030e983167d711751f30aea8d3ba85cfb
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-02 15:16:07 +0000

    fixed frankenSplice

--- Javascript_Algorithms_And_Data_Structures Certification/Basic Algorithm Scripting/frankenSplice.js
+++ Javascript_Algorithms_And_Data_Structures Certification/Basic Algorithm Scripting/frankenSplice.js
@@ -12,7 +12,7 @@
 * @param `number` - The index to be inserted at
 * @returns `array` - The new array
 */
-function frankenSplice(arr1, arr2, n) {
+export const frankenSplice = (arr1, arr2, n) => {
     let arr3 = arr2.slice()
     arr3.splice(n, 0, ...arr1)
     return arr3
