5fdc8287ab9c71d404cfbed5d87e16de86bc840d
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-02 15:16:27 +0000

    fixed frankenSplice

--- Javascript_Algorithms_And_Data_Structures Certification/Basic Algorithm Scripting/frankenSplice.js
+++ Javascript_Algorithms_And_Data_Structures Certification/Basic Algorithm Scripting/frankenSplice.js
@@ -13,9 +13,9 @@
 * @returns `array` - The new array
 */
 export const frankenSplice = (arr1, arr2, n) => {
-    let arr3 = arr2.slice()
-    arr3.splice(n, 0, ...arr1)
-    return arr3
-  }
+  let arr3 = arr2.slice()
+  arr3.splice(n, 0, ...arr1)
+  return arr3
+}
 	
 	frankenSplice([1, 2], ["a", "b"], 1)
