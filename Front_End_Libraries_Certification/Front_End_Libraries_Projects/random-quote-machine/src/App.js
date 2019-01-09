9620c1b53210d0940b2008e392b5414021122b32
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-09 15:54:31 +0000

    Cleaned boilerplate

---
+++ Front_End_Libraries_Certification/Front_End_Libraries_Projects/random-quote-machine/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Random Quote Machine
+      </div>
+    )
+  }
+}
+
+export default App
