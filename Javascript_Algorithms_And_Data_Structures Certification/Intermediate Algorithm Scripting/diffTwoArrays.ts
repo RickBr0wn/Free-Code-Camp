f3db297232d6b314867b1790fec07d8eb4bd9bb7
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-16 00:12:56 +0000

    converted to typescript

--- Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/diffTwoArrays.ts
+++ Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/diffTwoArrays.ts
@@ -5,9 +5,9 @@
  * @param `array` - The second array to be checked
  * @returns `array` - A new array containing the symmetric difference of the two arrays
  */
-const diffArray = (arr1, arr2) =>
+const diffArray = (arr1: number[], arr2: number[]) =>
   arr1
     .concat(arr2)
     .filter(item => arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1)
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
