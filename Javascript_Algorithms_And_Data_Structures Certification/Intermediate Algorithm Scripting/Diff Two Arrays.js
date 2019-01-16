00096eccb7cfe5cbb671462bc0118dd5300c95bf
RickBr0wn <ricky.brown.00@gmail.com>
2019-01-16 00:00:07 +0000

    Updated Diff Of Two Arrays

---
+++ Javascript_Algorithms_And_Data_Structures Certification/Intermediate Algorithm Scripting/Diff Two Arrays.js
@@ -0,0 +1,13 @@
+/**
+ * A function to compare two arrays. It will return a new array with any of the items only found in one of the two given arrays, but not both.
+ * In othre words, it return the symmetric difference of the two arrays.
+ * @param `array` - The first array to be checked
+ * @param `array` - The second array to be checked
+ * @returns `array` - A new array containing the symmetric difference of the two arrays
+ */
+const diffArray = (arr1, arr2) =>
+  arr1
+    .concat(arr2)
+    .filter(item => arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1)
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
