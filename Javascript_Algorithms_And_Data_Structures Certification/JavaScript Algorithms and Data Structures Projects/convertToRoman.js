7b8d76bf1a1f6f429e05e616f6cda38c36170b2a
RiCK BROWN <ricky.brown.00@gmail.com>
2019-01-31 00:43:29 +0000

    Updated Roman Numeral

--- Javascript_Algorithms_And_Data_Structures Certification/JavaScript Algorithms and Data Structures Projects/convertToRoman.js
+++ Javascript_Algorithms_And_Data_Structures Certification/JavaScript Algorithms and Data Structures Projects/convertToRoman.js
@@ -1,36 +1,6 @@
-// function convertToRoman(num) {
-//   var decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-//   var romanNumeralArray = [
-//     'M',
-//     'CM',
-//     'D',
-//     'CD',
-//     'C',
-//     'XC',
-//     'L',
-//     'XL',
-//     'X',
-//     'IX',
-//     'V',
-//     'IV',
-//     'I'
-//   ]
-//   var answer = ''
-//   var test = 36 % 10
-
-//   for (var i = 0; i < decimalArray.length; i++) {
-//     while (num % decimalArray[i] < num) {
-//       answer += romanNumeralArray[i]
-//       num -= decimalArray[i]
-//     }
-//   }
-
-//   return answer
-// }
-
-const convertToRoman = num => {
-  const decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-  const romanNumeralArray = [
+function convertToRoman(num) {
+  var decimalArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var romanNumeralArray = [
     'M',
     'CM',
     'D',
@@ -45,17 +15,16 @@ const convertToRoman = num => {
     'IV',
     'I'
   ]
-
-  return decimalArray.map((decimal, index, array) => {
-    let answer
-    console.log(decimal)
-    while (num % decimal < num) {
-      num -= decimalArray[index]
-      answer += romanNumeralArray[index]
+  var answer = ''
+  for (var i = 0; i < decimalArray.length; i++) {
+    while (num % decimalArray[i] < num) {
+      answer += romanNumeralArray[i]
+      num -= decimalArray[i]
     }
-    return answer
-  })
+  }
+  return answer
 }
+
 
 convertToRoman(36)
 
